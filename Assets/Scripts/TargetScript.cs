96665e40d2410b1a5d6b387a0b6c18bfd0c71935
Colton <Coltoniredale@yahoo.com>
2016-07-30 20:41:41 -0700

    2w4tfgftg
    xrtdh

---
+++ Assets/Scripts/TargetScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetScript : MonoBehaviour {
+	Plane plane;
+	public Transform transformh;
+	void Start (){
+		plane = new Plane(Vector3.up, Vector3.zero);
+	}
+
+	void Update (){
+		if (Input.GetMouseButtonDown (0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			float fDist = 0.0f;
+			plane.Raycast (ray, out fDist); 
+			transform.position = ray.GetPoint (fDist);
+		}
+	}
+}
---
+++ Assets/Scripts/UnitScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitScript : MonoBehaviour {
+	public Transform target;
+	public float speed;
+	private bool atTar = false;
+	private bool isSpawned = false;
+	void Start () {
+		
+	}
+	void Update () {
+		Vector3 dir = target.transform.position - transform.position;
+		float angle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.up);
+		//transform.Translate (Vector3.forward);
+	}
+}
